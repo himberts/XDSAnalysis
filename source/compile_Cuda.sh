ebcbd2e775f11bf9a76472c8fea9cafa36fa6dad
himberts <himberts@mcmaster.ca>
2022-03-21 16:46:52 -0400

    Restructured Folders and compiled with LibGSL 2.5 and Cuda 11.2 and LibBoost 11.17

---
+++ source/compile_Cuda.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "Build Program ..."
+/usr/local/cuda-11.2/bin/nvcc -Wno-deprecated-gpu-targets -I/usr/local/include/gsl -L/usr/local/lib -lgsl libs/GraphicsLib.cu libs/CudaCoreFunctions.cu libs/diffusetoolbox.cpp libs/diffuselibCuda.cu diffuse_analysis_v21_CUDA.cu -o ../bin/diffuse_analysis_v21_CUDA -lgsl -lgslcblas -lm -lboost_program_options
+echo "Done"
