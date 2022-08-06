cff1fbb904fe25a01627f05edf0c70240159a5be
Sebastian Himbert <himbertsebastian@gmail.com>
2022-08-06 21:18:56 +0000

    Bug Fix in compilation for Jetstream 2

--- source/compile_Cuda.sh
+++ source/compile_Cuda.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 echo "Build Program ..."
-/usr/local/cuda-11.6/bin/nvcc --machine=64 -gencode=arch=compute_70,code=sm_70 -Wno-deprecated-gpu-targets -I/usr/local/include/gsl -L/usr/local/lib -lgsl libs/GraphicsLib.cu libs/CudaCoreFunctions.cu libs/diffusetoolbox.cpp libs/diffuselibCuda.cu diffuse_analysis_v21_CUDA.cu -o ../bin/diffuse_analysis_v21_CUDA -lgsl -lgslcblas -lm -lboost_program_options
+nvcc -I/usr/local/include/gsl -L/usr/local/lib -lgsl libs/GraphicsLib.cu libs/CudaCoreFunctions.cu libs/diffusetoolbox.cpp libs/diffuselibCuda.cu diffuse_analysis_v21_CUDA.cu -o ../bin/diffuse_analysis_v21_CUDA -lgsl -lgslcblas -lm -lboost_program_options
 echo "Done"
