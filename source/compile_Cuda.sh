b0f72612dc0e73bcde5d3a52969a4060a9a00100
Sebastian Himbert <63553758+himberts@users.noreply.github.com>
2022-09-01 06:51:37 -0400

    Update compile_Cuda.sh

--- source/compile_Cuda.sh
+++ source/compile_Cuda.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 echo "Build Program ..."
-nvcc -I/usr/local/include/gsl -L/usr/local/lib -lgsl libs/GraphicsLib.cu libs/CudaCoreFunctions.cu libs/diffusetoolbox.cpp libs/diffuselibCuda.cu diffuse_analysis_v22_CUDA.cu -o ../bin/diffuse_analysis_v22_CUDA -lgsl -lgslcblas -lm -lboost_program_options
+nvcc -I/usr/local/include/gsl -L/usr/local/lib -lgsl libs/GraphicsLib.cu libs/CudaCoreFunctions.cu libs/diffusetoolbox.cpp libs/diffuselibCuda.cu diffuse_analysis_v23_CUDA.cu -o ../bin/diffuse_analysis_v23_CUDA -lgsl -lgslcblas -lm -lboost_program_options
 echo "Done"
